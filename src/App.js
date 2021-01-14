9b409a66d4688bdd0e18965f6660db0395cc58ff
capre15 <69794683+capre15@users.noreply.github.com>
2021-01-14 17:30:28 -0500

    added return result

--- src/App.js
+++ src/App.js
@@ -1,52 +1,16 @@
-
-import './App.css';
-import React,{Component} from 'react'
-import MatchCity from './Components/MatchCity'
-import axios from 'axios'
+import "./App.css";
+import React, { Component } from "react";
+import "./Components/MatchCity.css";
+import MatchCity from "./Components/MatchCity";
 
 class App extends Component {
-  constructor(){
-    super()
-    this.state={
-      cityname: 'new york',
-      result: []
-    }
-    this.handleChange = this.handleChange.bind(this)
+  render() {
+    return (
+      <div className="App">
+        <MatchCity />
+      </div>
+    );
   }
-
-  handleChange(e){
-     
-    this.setState({
-      cityname: e.target.value
-    })
-  }
-  async componentDidMount() {
-    let name= this.state.cityname
-    const url = "http://ctp-zip-api.herokuapp.com/city/"+name.toUpperCase()
-    axios.get(url)
-      .then(res => {
-        const response = res.data;
-        this.setState({ 
-            result : response,
-        });
-      }).catch(console.error())
-}
-  render(){
-    
-      return (
-        <div className='App'>
-        
-          <h1> City name Search</h1>
-          <label name="citymane"> City Name:</label>
-          <input name="cityname" placeholder="New York" value={this.state.cityname} onChange={this.handleChange}/>
-          <button  onClick={(e)=>{if(e.key === 'Enter') this.componentDidMount()}}>Search</button>
-          {/* <MatchCity cityname={this.state.result}/> */}
-    
-        </div>
-      );
-  }
-
 }
 
 export default App;
-
--- src/Components/MatchCity.jsx
+++ src/Components/MatchCity.jsx
@@ -1,44 +1,53 @@
-import React, {Component} from 'react'
+import React, { Component } from "react";
+import axios from "axios";
 
+class MatchCity extends Component {
+  constructor() {
+    super();
+    this.state = {
+      cityname: null,
+      zipcode: [],
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
 
+  handleChange(e) {
+    let cityname = e.target.value;
+    let cityInput = cityname.toUpperCase();
 
-class MatchCity extends Component{
-    constructor(props){
-        super(props)
-
-        this.state ={
-            result : this.props.result
+    const url = `http://ctp-zip-api.herokuapp.com/city/${cityInput}`;
+    axios
+      .get(url)
+      .then((res) => {
+        const response = res.data;
+        let zipArr = [];
+        for (const i in response) {
+          zipArr.push(response[i]);
         }
 
-    }
-
-
-    render(){
-        let i=0
-        return(
-            <div>
-                                 {
-                 this.state.result.map((item) =>{
-                     
-                   return (
-                    <diV >
-                      
-                      <p>Zipcode {i++}: {item}</p>
-
-                      <br/>
-                      <br/>
-                    </diV>
-
-                   );
-
-                   })    
-
-                 }
-            </div>
-
-        );
-    }
+        this.setState({
+          zipcode: zipArr,
+        });
+      })
+      .catch(console.error());
+  }
+  render() {
+    return (
+      <div className="App">
+        <h1> City name Search</h1>
+        <label name="citymane"> City Name:</label>
+        {/* <input name="cityname" placeholder="New York" value={this.state.cityname} onChange={this.handleChange}/>
+              <button  onClick={(e)=>{if(e.key === 'Enter') this.componentDidMount()}}>Search</button> */}
+        {/* <MatchCity cityname={this.state.result}/> */}
+        <input type="text" name="zipcode" onChange={this.handleChange}></input>
+        <ul className="no-bullets">
+          {this.state.zipcode.map((zipcode, i) => (
+            <li key={i}>{zipcode}</li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
 }
 
-
-export default MatchCity
+export default MatchCity;
